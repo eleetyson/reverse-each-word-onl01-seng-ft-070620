629447e828c9e6254392e47ecaf1d1fc673b07a8
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-07 19:36:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,13 @@ def reverse_each_word(str)
   words_arr.collect {|word| word.reverse}
   words_arr.join(' ')
 end
+
+=begin
+def reverse_each_word(str)
+  words_arr = str.split
+  words_arr.each do |word|
+    word.reverse!
+  end
+  words_arr.join(' ')
+end
+=end
